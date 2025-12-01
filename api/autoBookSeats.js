584426776b115525f6b8327d8db0f8e6f3aa7016
Neeraj Goswami <Neeraj.Goswami@motherson.com>
2025-12-01 11:39:56 +0530

    Added task Executed code setinterval 3

--- api/autoBookSeats.js
+++ api/autoBookSeats.js
@@ -1,11 +1,9 @@
 // api/autoBookSeats.js
-import moment from "moment-timezone";
+//import moment from "moment-timezone";
 //import sendBookings from "../lib/sendBookings.js"; // your logic file
 
 export default async function autoBookSeatsHandler(req, res) {
-  console.log("🔥 Auto booking triggered at:", moment().tz("Asia/Kolkata").format());
-
+  console.log("🔥 Auto booking triggered at");
   //await sendBookings();
-
   res.status(200).json({ success: true });
 }
